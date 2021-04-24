68289c63d30052976b84cc29480066f0532acfdc
Roman Semykin <KindPanic@mail.ru>
2021-04-24 10:20:40 +0300

    1.5. Поля объекта [#518219]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,7 +1,30 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println("Кот, с именем " + this.name + ", съел " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick){
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
+        Cat gav = new Cat();
+        Cat black = new Cat();
+        gav.giveNick("gav");
+        black.giveNick("black");
+        gav.eat("kotletka");
+        black.eat("fish");
+        gav.show();
+        black.show();
     }
 }
