a8ed389e5fa05549735655685979ed6b52b69352
Roman Semykin <KindPanic@mail.ru>
2021-04-27 00:05:56 +0300

    2. Модель данных. [#518237]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Ivanov Ivan Ivanovich");
+        student.setGroup("OIT-405");
+        student.setReceiptDate(new Date());
+
+        System.out.println("Студент " + student.getFullName() + ", учащийся в группе " + student.getGroup()
+                + ", поступил " + student.getReceiptDate());
+    }
+}
