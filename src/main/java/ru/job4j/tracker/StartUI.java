c39ab7f9eae3778f28b55084c4cda1f615037b69
Roman Semykin <KindPanic@mail.ru>
2021-04-27 20:48:49 +0300

    5. Tracker - хранилище [#518246]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,15 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("number 1"));
+        tracker.add(new Item("Report 1"));
+        tracker.add(new Item("Report 2"));
+        tracker.add(new Item("Report 3"));
+        tracker.add(new Item("Report 1"));
+        tracker.add(new Item("Report 3"));
+        tracker.add(new Item("Report 1"));
+
+        System.out.println(tracker.findById(1));
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] Items = new Item[100];
     private int ids = 1;
@@ -9,6 +12,20 @@ public class Tracker {
         item.setId(ids++);
         Items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll(){
+        return Arrays.copyOf(this.Items,size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = findAll();
+        int index = 0;
+        for (int i = 0; i < rsl.length; i++) {
+            if (findAll()[i].getName().equals(key))
+                rsl[index++] = rsl[i];
+        }
+        return Arrays.copyOf(rsl, index);
     }
 
     public Item findById(int id) {
