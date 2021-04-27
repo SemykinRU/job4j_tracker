c5b184e041865c4996ad8f6f29dcaa2a98352605
Roman Semykin <KindPanic@mail.ru>
2021-04-27 18:24:09 +0300

    3. Массивы и модели. [#518238]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,26 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book[] arrayBook = {new Book("Евгений Онегин", 140),new Book("Ревизор", 130),
+                            new Book("Библия", 1768), new Book("Clean code", 1)};
+
+        for (int i = 0; i < arrayBook.length; i++) {
+            System.out.println(arrayBook[i].getName() + " - " + arrayBook[i].getCount() + "стр.");
+        }
+
+        Book temp = arrayBook[0];
+        arrayBook[0] = arrayBook[3];
+        arrayBook[3] = temp;
+
+        for (int i = 0; i < arrayBook.length; i++) {
+            System.out.println(arrayBook[i].getName() + " - " + arrayBook[i].getCount() + "стр.");
+        }
+
+        for (int i = 0; i < arrayBook.length; i++) {
+            if (arrayBook[i].getName().equals("Clean code"))
+            System.out.println(arrayBook[i].getName() + " - " + arrayBook[i].getCount() + "стр.");
+        }
+    }
+}
