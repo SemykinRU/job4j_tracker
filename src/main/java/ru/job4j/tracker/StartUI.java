f581086695dca5755728d577773d32faa9e09b48
Roman Semykin <KindPanic@mail.ru>
2021-04-29 20:55:44 +0300

    2.1. Реализация класса StartUI [#518179]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,68 +8,51 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu();
-            switch (Integer.parseInt(scanner.nextLine())) {
-                case 0 -> {
-                    System.out.println("=== Создаем новую заявку ====");
-                    System.out.print("Введите имя заявки: ");
-                    tracker.add(new Item(scanner.nextLine()));
+            int i = Integer.parseInt(scanner.nextLine());
+            if (i == 0) {
+                System.out.println("=== Создаем новую заявку ====");
+                System.out.print("Введите имя заявки: ");
+                tracker.add(new Item(scanner.nextLine()));
+            } else if (i == 1) {
+                for (Item item : tracker.findAll()) {
+                    System.out.println(item);
                 }
-                case 1 -> {
-                    for (Item item : tracker.findAll()) {
-                        System.out.println(item);
-                    }
-                    System.out.println("==== Нажмите Enter для продолжения ====");
-                    scanner.nextLine();
+            } else if (i == 2) {
+                System.out.print("Введите ID изменяемой заявки: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Введите имя заявки: ");
+                if (tracker.replace(id, new Item(scanner.nextLine()))) {
+                    System.out.println("Успешно.");
+                } else {
+                    System.out.println("Ошибка. Заявка с таким ID отсутствует.");
                 }
-                case 2 -> {
-                    for (Item item : tracker.findAll()) {
-                        System.out.println(item);
-                    }
-                    System.out.print("Введите ID изменяемой заявки: ");
-                    int id = Integer.parseInt(scanner.nextLine());
-                    System.out.print("Введите имя заявки: ");
-                    if (tracker.replace(id, new Item(scanner.nextLine()))) {
-                        System.out.println("Успешно.");
-                    } else {
-                        System.out.println("Ошибка. Заявка с таким ID отсутствует.");
-                    }
+            } else if (i == 3) {
+                System.out.print("Введите ID удаляемой заявки: ");
+                if (tracker.delete(Integer.parseInt(scanner.nextLine()))) {
+                    System.out.println("Успешно");
+                } else {
+                    System.out.println("Ошибка. Заявка с таким ID отсутствует.");
                 }
-                case 3 -> {
-                    for (Item item : tracker.findAll()) {
-                        System.out.println(item);
-                    }
-                    System.out.print("Введите ID удаляемой заявки: ");
-                    if (tracker.delete(Integer.parseInt(scanner.nextLine()))) {
-                        System.out.println("Успешно");
-                    } else {
-                        System.out.println("Ошибка. Заявка с таким ID отсутствует.");
-                    }
+            } else if (i == 4) {
+                System.out.print("Введите ID заявки: ");
+                Item item = tracker.findById(Integer.parseInt(scanner.nextLine()));
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Заявка с таким ID отсутствует.");
                 }
-                case 4 -> {
-                    System.out.print("Введите ID заявки: ");
-                    Item item = tracker.findById(Integer.parseInt(scanner.nextLine()));
-                    if (item != null) {
-                        System.out.println(item);
-                    } else {
-                        System.out.println("Заявка с таким ID отсутствует.");
+            } else if (i == 5) {
+                System.out.print("Введите имя заявки: ");
+                Item[] items = tracker.findByName(scanner.nextLine());
+                if (items.length > 0) {
+                    for (Item value : items) {
+                        System.out.println(value);
                     }
-                    System.out.println("==== Нажмите Enter для продолжения ====");
-                    scanner.nextLine();
+                } else {
+                    System.out.println("Заявка с таким именем не найдена.");
                 }
-                case 5 -> {
-                    System.out.print("Введите имя заявки: ");
-                    Item[] items = tracker.findByName(scanner.nextLine());
-                    if (items.length > 0) {
-                        for (Item value : items) {
-                            System.out.println(value);
-                        }
-                    } else {
-                        System.out.println("Заявка с таким именем не найдена.");
-                    }
-                    System.out.println("==== Нажмите Enter для продолжения ====");
-                    scanner.nextLine();
-                }
-                case 6 -> run = false;
+            } else if (i == 6) {
+                run = false;
             }
         }
     }
