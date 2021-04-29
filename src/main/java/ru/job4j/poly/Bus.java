83262732da51c3585c4e9f0106ee11da90a25a00
Roman Semykin <KindPanic@mail.ru>
2021-04-29 22:51:51 +0300

    3. Интерфейс Input [#518162]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,20 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void goIn() {
+        System.out.println("Поехали!");
+    }
+
+    @Override
+    public void passengers(int count) {
+        if (count > 0) {
+           this.goIn();
+        }
+    }
+
+    @Override
+    public double refuel(double value) {
+        return value * 46.6;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void goIn();
+    void passengers(int count);
+    double refuel(double value);
+}
