816747cc4dd126e9f404d969c5ce5e25d7de64a4
Roman Semykin <KindPanic@mail.ru>
2021-05-19 23:10:30 +0300

    0. Map, HashMap [#518147]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("semykinru@yandex.ru", "Semykin Roman");
+        map.put("semykinru@mail.ru", "Semykin Roman");
+        map.put("semykinru@rambler.ru", "Semykin Roman");
+        map.put("ivanovii@yandex.ru", "Ivanov Ivan");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
