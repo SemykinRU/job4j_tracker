016432a493af7d1025c4dac488ecd43a859abe56
Roman Semykin <KindPanic@mail.ru>
2021-05-21 23:10:57 +0300

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,9 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public class Item {
+public class Item implements Comparable<Item> {
 
     private int id;
     private String name;
@@ -43,5 +44,23 @@ public class Item {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public int compareTo(Item o) {
+        return Integer.compare(id, o.id);
     }
 }
---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    @Test
+    public void whenSortByID () {
+        List<Item> actual = Arrays.asList(
+                new Item(3, "item 3"),
+                new Item(1, "item 1"),
+                new Item(10, "item 10"),
+                new Item(6, "item 6")
+        );
+       List<Item> expected = Arrays.asList(
+               new Item(1, "item 1"),
+               new Item(3, "item 3"),
+               new Item(6, "item 6"),
+               new Item(10, "item 10")
+       );
+       actual.sort(new SortItemByID());
+       assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenReversSortByID () {
+        List<Item> actual = Arrays.asList(
+                new Item(3, "item 3"),
+                new Item(1, "item 1"),
+                new Item(10, "item 10"),
+                new Item(6, "item 6")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item(10, "item 10"),
+                new Item(6, "item 6"),
+                new Item(3, "item 3"),
+                new Item(1, "item 1")
+        );
+        actual.sort(new ReversSortItemByID());
+        assertThat(actual, is(expected));
+    }
+}
