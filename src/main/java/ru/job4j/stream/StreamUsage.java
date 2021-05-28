f107c5dc375c204b6aa87a8ee2e882a00839a571
Roman Semykin <KindPanic@mail.ru>
2021-05-29 00:17:26 +0300

    0.1. Отладка лямбда в IDEA [#259165]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -1,18 +1,28 @@
 package ru.job4j.stream;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamUsage {
-    public static void main(String[] args) {
-        List<Integer> number = Arrays.asList(1, 2, 6,
-                3, 5, 10);
-        List<Integer> sortEvenNumbers = number.stream()
-                                            .filter(x -> x % 2 == 0)
-                                            .collect(Collectors.toList());
-        for (Integer item : sortEvenNumbers) {
-            System.out.println(item);
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
         }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
+        );
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent == 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
     }
 }
