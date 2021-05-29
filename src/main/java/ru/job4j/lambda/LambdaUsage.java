e1ba8b3354b7a8ad8814c6e91e6398ab715c98d1
Roman <semykinru@yandex.ru>
2021-05-29 18:09:31 +0300

    2.2. Лямбда блок [#249208 #161726]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,23 +1,15 @@
 package ru.job4j.lambda;
 
-import java.util.*;
+import java.util.Comparator;
 
 public class LambdaUsage {
     public static void main(String[] args) {
-        String[] numbers = {
-                "1. Task",
-                "2. Task",
-                "11. Task"
+        Comparator<String> comDescSize = (left, right) -> {
+            System.out.println("Строка с размером "
+                    + right.length()
+                    + " меняется со стройкой с размером "
+                    + left.length());
+            return right.length() - left.length();
         };
-        Comparator<String> numSrt = (left, right) -> {
-            int first = Integer.valueOf(right.substring(0, right.indexOf(".")));
-            int second = Integer.valueOf(left.substring(0, left.indexOf(".")));
-            return first - second;
-        };
-        List<String> listNumbers = new ArrayList<>(Arrays.asList(numbers));
-        listNumbers.sort(numSrt);
-        for (String item : listNumbers) {
-            System.out.println(item);
-        }
     }
 }
