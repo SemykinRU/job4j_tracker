54c53a7efca2ae619c5aa0e8d9fdbe01c44ad1a5
Roman Semykin <KindPanic@mail.ru>
2021-06-01 19:46:33 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,51 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(x -> x.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D);
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(x -> new Tuple(x.getName(),
+                x.getSubjects().stream()
+                        .mapToInt(Subject::getScore).average()
+                        .orElse(0D)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.flatMap(x -> x.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName, Collectors.averagingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(item -> new Tuple(item.getKey(),
+                                        item.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(x -> new Tuple(x.getName(),
+                x.getSubjects().stream()
+                        .mapToInt(Subject::getScore).sum()))
+                .max(Comparator.comparingDouble(Tuple::getScore))
+                .orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(x -> x.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName, Collectors.summingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(item -> new Tuple(item.getKey(),
+                        item.getValue()))
+                .max(Comparator.comparingDouble(Tuple::getScore))
+                .orElse(null);
+    }
+}
